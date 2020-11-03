9c78413403138adc4c8aa5c68afec1edf3fb8ca1
Jonas <53447664+Lebedeu@users.noreply.github.com>
2020-11-03 18:43:31 -0300

    Cors

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,13 @@ var cors = require('cors');
 
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
 });
 
 // view engine setup
