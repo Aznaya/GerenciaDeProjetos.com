30f11c05fffe5e2acd6127d84c6bb05267189013
Jonas <53447664+Lebedeu@users.noreply.github.com>
2020-11-04 17:24:57 -0300

    Atualização do CORS

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     app.use(cors());
     next();
 });
